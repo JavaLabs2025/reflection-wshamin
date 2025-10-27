b1eafa8f1f0f3bb59a81db0f4ec162f6a2c429f3
Viktor Shamin <v.shamin@corp.mail.ru>
2025-10-27 20:50:26 +0300

    Рабочий прототип (версия 3)

--- src/main/java/org/example/generator/Generator.java
+++ src/main/java/org/example/generator/Generator.java
@@ -17,20 +17,21 @@ public class Generator {
     private static final int MAX_DEPTH = 3;
     private final Random rnd = new Random();
 
-    public <T> T generateValueOfType(Class<T> type) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        return (T) generate(type, 0, new HashMap<>());
+    public <T> T generateValueOfType(Class<T> type)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        return (T) generate(type, 0);
     }
 
-    private Object generate(Class<?> type, int depth, Map<Class<?>, Integer> visiting) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    private Object generate(Class<?> type, int depth)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
 
         if (depth > MAX_DEPTH) return defaultFor(type);
-        visiting.merge(type, 1, Integer::sum);
 
         if (type.isPrimitive()) return randomPrimitive(type);
         if (isWrapper(type)) return randomWrapper(type);
         if (type == String.class) return randomString();
         if (type.isEnum()) return randomEnum(type);
-        if (type.isArray()) return randomArray(type.getComponentType(), depth, visiting);
+        if (type.isArray()) return randomArray(type.getComponentType(), depth);
         if (Collection.class.isAssignableFrom(type)) return new ArrayList<>();
 
         if (type.isInterface() || Modifier.isAbstract(type.getModifiers())) {
@@ -40,56 +41,48 @@ public class Generator {
                 throw new IllegalArgumentException("Нет @Generatable реализаций для: " + type.getName());
             }
 
-            return generate(impl, depth + 1, visiting);
+            return generate(impl, depth + 1);
         }
 
         if (!type.isAnnotationPresent(Generatable.class)) {
             throw new IllegalArgumentException("Класс не помечен @Generatable: " + type.getName());
         }
 
-        try {
-            Constructor<?>[] constructors = type.getDeclaredConstructors();
+        Constructor<?>[] constructors = type.getDeclaredConstructors();
 
-            for (Constructor<?> c : constructors) {
-                Class<?>[] params = c.getParameterTypes();
-                Type[] gParams = c.getGenericParameterTypes();
-                Object[] args = new Object[params.length];
+        for (Constructor<?> c : constructors) {
+            Class<?>[] params = c.getParameterTypes();
+            Type[] gParams = c.getGenericParameterTypes();
+            Object[] args = new Object[params.length];
 
-                boolean ok = true;
+            boolean ok = true;
 
-                for (int i = 0; i < params.length; i++) {
-                    Class<?> p = params[i];
-                    try {
-                        if (Collection.class.isAssignableFrom(p)) {
-                            args[i] = createAndFillCollection(p, gParams[i], depth, visiting);
-                        } else {
-                            args[i] = generate(p, depth + 1, visiting);
-                        }
-                    } catch (Throwable t) {
-                        ok = false;
-                        break;
-                    }
-                }
-
-                if (!ok) {
-                    continue;
-                }
-
+            for (int i = 0; i < params.length; i++) {
+                Class<?> p = params[i];
                 try {
-                    return c.newInstance(args);
-                } catch (InvocationTargetException e) {
-                    System.out.println("Конструктор не подошел, пробуем следующий");
+                    if (Collection.class.isAssignableFrom(p)) {
+                        args[i] = createAndFillCollection(p, gParams[i], depth);
+                    } else {
+                        args[i] = generate(p, depth + 1);
+                    }
+                } catch (Throwable t) {
+                    ok = false;
+                    break;
                 }
             }
 
-            return defaultFor(type);
-        } finally {
-            visiting.merge(type, -1, Integer::sum);
+            if (!ok) {
+                continue;
+            }
 
-            if (visiting.get(type) != null && visiting.get(type) <= 0) {
-                visiting.remove(type);
+            try {
+                return c.newInstance(args);
+            } catch (InvocationTargetException e) {
+                System.out.println("Конструктор не подошел, пробуем следующий");
             }
         }
+
+        return defaultFor(type);
     }
 
     private Object randomPrimitive(Class<?> p) {
@@ -126,18 +119,19 @@ public class Generator {
         return constants[rnd.nextInt(constants.length)];
     }
 
-    private Object randomArray(Class<?> component, int depth, Map<Class<?>, Integer> visiting) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    private Object randomArray(Class<?> component, int depth)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
         int len = rnd.nextInt(3);
         Object array = Array.newInstance(component, len);
 
         for (int i = 0; i < len; i++) {
-            Array.set(array, i, generate(component, depth + 1, visiting));
+            Array.set(array, i, generate(component, depth + 1));
         }
 
         return array;
     }
 
-    private Collection<?> createAndFillCollection(Class<?> raw, Type gType, int depth, Map<Class<?>, Integer> visiting)
+    private Collection<?> createAndFillCollection(Class<?> raw, Type gType, int depth)
             throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Collection<Object> coll;
 
@@ -163,7 +157,7 @@ public class Generator {
         int size = 1 + rnd.nextInt(3);
 
         for (int k = 0; k < size; k++) {
-            Object val = generate(elemClass, depth + 1, visiting);
+            Object val = generate(elemClass, depth + 1);
             coll.add(val);
         }
 
